5374053982d6f3f3b2872719606625d5b3da5699
vortex-ninja <vortex.ninja@protonmail.com>
2017-02-24 21:49:41 +0100

    bug: Correct name of the class attribute
    You tube trailer attribute of Movie class had different
    names in class definition and in fresh_tomatoes.py file.

--- media.py
+++ media.py
@@ -4,11 +4,11 @@ import webbrowser
 
 
 class Movie():
-    def __init__(self, title, storyline, poster_image_url, yt_trailer_url):
+    def __init__(self, title, storyline, poster_image_url, trailer_youtube_url):
         self.title = title
         self.storyline = storyline
         self.poster_image_url = poster_image_url
-        self.yt_trailer_url = yt_trailer_url
+        self.trailer_youtube_url = trailer_youtube_url
 
     def show_trailer(self):
-        webbrowser.open(self.yt_trailer_url)
+        webbrowser.open(self.trailer_youtube_url)
