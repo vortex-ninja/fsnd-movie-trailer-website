3187bfa897f587056a59ee8cd3d3274165f1018d
vortex-ninja <vortex.ninja@protonmail.com>
2017-02-24 23:59:14 +0100

    feat: Add IMDb API support

--- media.py
+++ media.py
@@ -1,14 +1,21 @@
 """Class definition for movie object"""
 
 import webbrowser
+import imdb
 
 
 class Movie():
-    def __init__(self, title, storyline, poster_image_url, trailer_youtube_url):
+    """test doc string"""
+    def __init__(self, title, storyline, trailer_youtube_url, imdb_id):
+
+        imdb_access = imdb.IMDb()
+        movie = imdb_access.get_movie(str(imdb_id))
+
         self.title = title
         self.storyline = storyline
-        self.poster_image_url = poster_image_url
+        self.poster_image_url = movie['full-size cover url']
         self.trailer_youtube_url = trailer_youtube_url
+        self.rating = movie['rating']
 
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
