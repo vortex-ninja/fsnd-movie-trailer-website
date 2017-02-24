ac6bf0ebb0730cf05be9f3ac27e0b64f4bd6f825
vortex-ninja <vortex.ninja@protonmail.com>
2017-02-24 21:20:46 +0100

    feat: Add show_trailer method to Movie class

--- media.py
+++ media.py
@@ -1,5 +1,7 @@
 """Class definition for movie object"""
 
+import webbrowser
+
 
 class Movie():
     def __init__(self, title, storyline, poster_image_url, yt_trailer_url):
@@ -7,3 +9,6 @@ class Movie():
         self.storyline = storyline
         self.poster_image_url = poster_image_url
         self.yt_trailer_url = yt_trailer_url
+
+    def show_trailer(self):
+        webbrowser.open(self.yt_trailer_url)
