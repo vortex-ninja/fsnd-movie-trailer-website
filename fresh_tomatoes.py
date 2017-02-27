04aabeb6fceb329cea0c178506aa9fb47a85ae3e
vortex-ninja <vortex.ninja@protonmail.com>
2017-02-27 18:39:20 +0100

    fix: Change rendered file encoding to utf-8

--- fresh_tomatoes.py
+++ fresh_tomatoes.py
@@ -1,6 +1,7 @@
 import webbrowser
 import os
 import jinja2
+import codecs
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir),
@@ -13,15 +14,13 @@ def render_template(template, **params):
 
 
 def open_movies_page(movies):
-    # Create or overwrite the output file
-    output_file = open('fresh_tomatoes.html', 'w')
 
-
-    # Output the file
-
-    output_file.write(render_template('main_page_head.html') + render_template('main_page_content.html', movies=movies))
-    output_file.close()
+    # create a file and render templates in it
+    with codecs.open('fresh_tomatoes.html', 'w', encoding="utf-8") as output_file:
+        output_file.write(render_template('main_page_head.html') +
+                          render_template('main_page_content.html', movies=movies))
 
     # open the output file in the browser (in a new tab, if possible)
     url = os.path.abspath(output_file.name)
     webbrowser.open('file://' + url, new=2)
+
