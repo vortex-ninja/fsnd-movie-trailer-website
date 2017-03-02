8221f405627f878bb5f52c7c78ae4d10501e3449
vortex-ninja <vortex.ninja@protonmail.com>
2017-03-02 19:41:22 +0100

    refactor: Wrap getting imdb info in a function
    Also use dictionary to prepare attribute, value pairs
    for the Movie object

--- media.py
+++ media.py
@@ -8,75 +8,74 @@ class Movie():
     """test doc string"""
     def __init__(self, **kwargs):
 
-        imdb_url = None
-        title = None
-        storyline = None
-        trailer_youtube_url = None
-        trailer_youtube_id = None
-        rating = None
-        year = None
-        poster_image_url = None
+        # a dictionary that will be used to set object's instance attributes
+        attributes = {}
 
-        def get_imdb_id():
+        def get_imdb_id(kwargs):
+            """extracts imdb id if possible"""
+
             imdb_id = None
             if 'imdb_id' in kwargs:
                 imdb_id = kwargs['imdb_id']
-            
-            # Extract movie ID from url
+
+            # extracts movie ID from url
             if 'imdb_url' in kwargs and not imdb_id:
                 imdb_url = kwargs['imdb_url']
                 imdb_id_match = re.search(r'(?<=tt)[0-9]+', imdb_url)
                 imdb_id = imdb_id_match.group(0) if imdb_id_match else None
+
             return imdb_id
 
-        imdb_id = get_imdb_id()
+        def get_youtube_id(kwargs):
+            """extracts the youtube ID from the url"""
 
-        # Access movie object through IMDb API
+            trailer_youtube_id = None
+            if 'trailer_youtube_url' in kwargs:
+                trailer_youtube_url = kwargs['trailer_youtube_url']
+                youtube_id_match = re.search(r'(?<=v=)[^&#]+', trailer_youtube_url)
+                youtube_id_match = youtube_id_match or re.search(r'(?<=be/)[^&#]+',
+                                                             trailer_youtube_url)
+                trailer_youtube_id = (youtube_id_match.group(0)
+                                      if youtube_id_match else None)
 
-        if imdb_id:
+            return trailer_youtube_id
+
+        def get_imdb_info(imdb_id):
+            """extracts imdb info through imdb api"""
+
+            attrs = {'imdb_id': imdb_id}
             imdb_access = imdb.IMDb()
             movie = imdb_access.get_movie(imdb_id)
             if 'title' in movie.keys():
-                title = movie['title']
+                attrs['title'] = movie['title']
             if 'plot outline' in movie.keys():
-                storyline = movie['plot outline']
+                attrs['storyline'] = movie['plot outline']
             if 'rating' in movie.keys():
-                rating = movie['rating']
+                attrs['rating'] = movie['rating']
             if 'year' in movie.keys():
-                year = movie['year']
+                attrs['year'] = movie['year']
             if 'full-size cover url' in movie.keys():
-                poster_image_url = movie['full-size cover url']
+                attrs['poster_image_url'] = movie['full-size cover url']
             else:
                 if 'cover_url' in movie.keys():
-                    poster_image_url = movie['cover url']
-            imdb_url = "http://www.imdb.com/title/tt" + imdb_id + "/"
+                    attrs['poster_image_url'] = movie['cover url']
+            attrs['imdb_url'] = "http://www.imdb.com/title/tt" + imdb_id + "/"
 
-        # Extract the youtube ID from the url
+            return attrs
 
-        if 'trailer_youtube_url' in kwargs:
-            trailer_youtube_url = kwargs['trailer_youtube_url']
-            youtube_id_match = re.search(r'(?<=v=)[^&#]+', trailer_youtube_url)
-            youtube_id_match = youtube_id_match or re.search(r'(?<=be/)[^&#]+',
-                                                             trailer_youtube_url)
-            trailer_youtube_id = (youtube_id_match.group(0)
-                                  if youtube_id_match else None)
+        imdb_id = get_imdb_id(kwargs)
 
+        if imdb_id:
+            attributes.update(get_imdb_info(imdb_id))
 
-        if 'title' in kwargs:
-            title = kwargs['title']
-        if 'storyline' in kwargs:
-            storyline = kwargs['storyline']
+        trailer_youtube_id = get_youtube_id(kwargs)
 
-        self.imdb_id = imdb_id
-        self.imdb_url = imdb_url
-        self.title = title
-        self.storyline = storyline
-        self.trailer_youtube_url = trailer_youtube_url
-        self.poster_image_url = poster_image_url
-        self.rating = rating
-        self.trailer_youtube_id = trailer_youtube_id
-        self.year = year
+        if trailer_youtube_id:
+            attributes['trailer_youtube_id'] = trailer_youtube_id
 
-        print "[+] Movie '" + self.title + "' object created"
+        attributes.update(kwargs)
 
+        for key, value in attributes.items():
+            setattr(self, key, value)
 
+        print "Movie '%s' object created" % getattr(self, 'title', 'title missing')
