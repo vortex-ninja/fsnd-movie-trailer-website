ba8239826268eba26a141944b52cd98175875c6c
vortex-ninja <vortex.ninja@protonmail.com>
2017-03-05 14:24:51 +0100

    chore: Prepare entertainment_center.py file

--- entertainment_center.py
+++ entertainment_center.py
@@ -2,48 +2,41 @@ import fresh_tomatoes
 import media
 
 
-big_lebowski = media.Movie("Big Lebowski",
-                           "A flick bout a dude whose rug is stolen.",
-                           "https://www.youtube.com/watch?v=cd-go0oBF4Y",
-                           "http://www.imdb.com/title/tt0118715/?ref_=nv_sr_1")
+big_lebowski = media.Movie(title="The Big Lebowski",
+                           storyline=u"A flick bout a dude whose rug is stolen.",
+                           trailer_youtube_url="https://www.youtube.com/watch?v=cd-go0oBF4Y",
+                           imdb_url="http://www.imdb.com/title/tt0118715/?ref_=nv_sr_1")
 
-the_game = media.Movie("The Game",
-                       "A bored with life guy gets to play an interesting game.",
-                       "https://www.youtube.com/watch?v=0kqQNBR09Rc",
-                       "http://www.imdb.com/title/tt0119174/?ref_=fn_al_tt_1")
+the_game = media.Movie(imdb_url="http://www.imdb.com/title/tt0119174/",
+                       storyline="A bored with life guy gets to play an interesting game.",
+                       trailer_youtube_url="https://www.youtube.com/watch?v=0kqQNBR09Rc")
 
-clean_shaven = media.Movie("Clean, Shaven",
-                           "World from a schizophrenic POV",
-                           "https://www.youtube.com/watch?v=6aInRjIwjpU",
-                           "http://www.imdb.com/title/tt0106579/?ref_=nv_sr_1")
+clean_shaven = media.Movie(trailer_youtube_url="https://www.youtube.com/watch?v=6aInRjIwjpU",
+                           imdb_id='0106579')
 
-braindead = media.Movie("Braindead",
-                        "Gore comedy classic from Peter Jackson.",
-                        "https://www.youtube.com/watch?v=O8LIug1cP04",
-                        "http://www.imdb.com/title/tt0103873/?ref_=fn_al_tt_1")
+memento = media.Movie(title=u"Memento",
+                      imdb_url="http://www.imdb.com/title/tt0209144/",
+                      trailer_youtube_url="https://www.youtube.com/watch?v=0vS0E9bBSL0")
 
-total_recall = media.Movie("Total Recall",
-                           "Guy has a sweet tooth for exciting vacation.",
-                           "https://www.youtube.com/watch?v=2DwNb-ZGVjE",
-                           "http://www.imdb.com/title/tt0100802/?ref_=nv_sr_1")
+twelve_monkeys = media.Movie(imdb_id="0114746",
+                             trailer_youtube_url="https://www.youtube.com/watch?v=wuggl3cZD8A")
 
-a_scanner_darkly = media.Movie("A Scanner Darkly",
-                               "Undercover operation against new and deadly drug.",
-                               "https://www.youtube.com/watch?v=hkjDUERgCQw",
-                               "http://www.imdb.com/title/tt0405296/?ref_=nv_sr_1")
+moon = media.Movie(imdb_id="1182345",
+                   trailer_youtube_url="https://www.youtube.com/watch?v=twuScTcDP_Q")
 
+empty_movie = media.Movie()
 
-movies = [big_lebowski, the_game, clean_shaven,
-          braindead, total_recall, a_scanner_darkly]
+movies = [big_lebowski, the_game, clean_shaven, memento, twelve_monkeys, moon,
+          empty_movie]
 
+# removes films that don't have at least 'title' and 'rating' attributes
+movies = fresh_tomatoes.filter_movies(movies, 'title', 'rating')
 
-# print big_lebowski.yt_trailer_id
+# sorts movies by imdb rating
+movies = fresh_tomatoes.sort_by_rating(movies)
 
-sorted_movies = sorted(movies, key=lambda x: x.rating, reverse=True)
+# creates a sublist of movies from IMDb Top 250 list
+# movies = fresh_tomatoes.between_years(1990, 2000, 9)
 
-
-fresh_tomatoes.open_movies_page(sorted_movies)
-
-
-# for movie in sorted_movies:
-#   print movie.title + ' ' + str(movie.rating)
+# renders and opens html file
+fresh_tomatoes.open_movies_page(movies)
