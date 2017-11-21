a3fd79a3552fb0c46541998f04c5c663bd96a8c9
vortex-ninja <vortex.ninja@protonmail.com>
2017-11-21 18:01:37 +0100

    Correct form validation

--- forms.py
+++ forms.py
@@ -1,19 +1,22 @@
 from flask_wtf import FlaskForm
 from wtforms import SubmitField, IntegerField
-from wtforms.validators import Required
+from wtforms.validators import NumberRange
+
+
+def msg_swap(form, field):
+    """Swaps error message for IntegerField check"""
+
+    msg_to_rpl = u'Not a valid integer value'
+    err_msg = u'Please enter a number'
+    print(form.min_year.data)
+    if msg_to_rpl in field.errors:
+        field.errors[field.errors.index(msg_to_rpl)] = err_msg
 
 
 class QueryForm(FlaskForm):
 
-    error_msg = 'Please input a number.'
-    min_year = IntegerField('start year:',
-                            default=1990,
-                            validators=[Required(message=error_msg)])
-    max_year = IntegerField('end year: ',
-                            default=9999,
-                            validators=[Required(message=error_msg)])
+    min_year = IntegerField('start year:', validators=[msg_swap])
+    max_year = IntegerField('end year:', validators=[msg_swap])
     number = IntegerField('number of movies:',
-                          default=6,
-                          validators=[Required(message=error_msg)])
-
+                          validators=[msg_swap, NumberRange(min=1, max=6)])
     submit = SubmitField('Submit')
